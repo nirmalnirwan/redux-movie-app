c8d77aea5fdf5923bde3db9e1262c5c511b0b103
Tharindu Nirmal <tns@tiqri.com>
2022-11-10 22:21:43 +0530

    integrated redux toolkit

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import './MovieCard.scss'
 
-const MovieCard = () => {
+const MovieCard = (props) => {
+    const { data } = props;
     return (
-        <div>
-            MovieCard
+        <div className='card-item'>
+            <div className='card-inner'>
+                <div className='card-top'>
+                    <img src={data.Poster} alt={data.Title}></img>
+                </div>
+                <div className='card-bottom'>
+                    <div className='card-info'>
+                       <h4>{data.Title}</h4>
+                       <p>{data.Year}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
 
-export default MovieCard;
+export default MovieCard; 
